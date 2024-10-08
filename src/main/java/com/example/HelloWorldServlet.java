d98f4071f4b2d38c8807aae3b52ba9a9976c2c25
DEEPAK N A <60418902+Deep7k@users.noreply.github.com>
2024-10-08 12:36:52 +0530

    edit3

--- src/main/java/com/example/HelloWorldServlet.java
+++ src/main/java/com/example/HelloWorldServlet.java
@@ -13,6 +13,6 @@ public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().println("<h1>Hello, World!!!. Lets check if webhook triggers. check email notification</h1>");
+        response.getWriter().println("<h1>Hello, World!!!. Lets check if webhook triggers. check email notification. today is tuesday</h1>");
     }
 }
